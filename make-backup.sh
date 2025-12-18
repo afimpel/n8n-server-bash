267f4c4ab5963617e5dd4181f37f791690670362
Alvaro Fimpel <afimpel@gmail.com>
2025-12-18 12:23:05 -0300

    feat: :art: add environment...

--- make-backup.sh
+++ make-backup.sh
@@ -2,23 +2,29 @@
 cd
 mkdir -p backup
 chmod 777 backup -Rv
-echo -e "\n\t------------\t hostname: $(hostname) \t date: $(date) \t------------\n"
+echo -e "\n\t------------\t EXPORT\t| hostname: $(hostname) \t date: $(date) \t------------\n"
+
 n8n export:workflow --all --output=backup/$(hostname)-workflows.json
 n8n export:workflow --backup --output=backup/$(hostname)-workflow/
+
+echo -e "\n\t------------\t RENAME\t| hostname: $(hostname) \t date: $(date) \t------------\n"
 for iface in $(ls backup/$(hostname)-workflow/); do
-    nameID=$(jq -r '.name + "-" + .id' backup/$(hostname)-workflow/$iface)
-    echo -e "\t- Processing workflow file: ${iface}.json to ${nameID/// /-}.json"
-    mv backup/$(hostname)-workflow/$iface backup/$(hostname)-workflow/${nameID/// /-}.json
+    nameID=$(jq -r '.name + "-" + .id' backup/$(hostname)-workflow/$iface | sed 's/ /-/g')
+    echo -e "\t-\t Processing workflow file: ${iface}.json to ${nameID}.json"
+    mv backup/$(hostname)-workflow/$iface backup/$(hostname)-workflow/${nameID}.json
 done
 
-echo -e "\n\t------------\t hostname: $(hostname) \t date: $(date) \t------------\n"
+echo -e "\n\t------------\t EXPORT\t| hostname: $(hostname) \t date: $(date) \t------------\n"
+
 n8n export:credentials --all --decrypted --output=backup/$(hostname)-credentials.json
 n8n export:credentials --backup --decrypted --output=backup/$(hostname)-credential/
+
+echo -e "\n\t------------\t RENAME\t| hostname: $(hostname) \t date: $(date) \t------------\n"
 for iface in $(ls backup/$(hostname)-credential/); do
-    nameID=$(jq -r '.name + "-" + .id' backup/$(hostname)-credential/$iface)
-    echo -e "\t- Processing credential file: ${iface}.json to ${nameID/// /-}.json"
-    mv backup/$(hostname)-credential/$iface backup/$(hostname)-credential/${nameID/// /-}.json
+    nameID=$(jq -r '.name + "-" + .id' backup/$(hostname)-credential/$iface | sed 's/ /-/g')
+    echo -e "\t-\t Processing credential file: ${iface}.json to ${nameID}.json"
+    mv backup/$(hostname)-credential/$iface backup/$(hostname)-credential/${nameID}.json
 done
 
-echo -e "\n\t------------\t hostname: $(hostname) \t date: $(date) \t------------\n"
+echo -e "\n\t------------\t END\t| hostname: $(hostname) \t date: $(date) \t------------\n"
 chmod 777 backup -Rv
