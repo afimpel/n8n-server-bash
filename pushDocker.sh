886db81d0621e0c3becb4032a192da5563ddb9aa
Alvaro Fimpel <afimpel@gmail.com>
2025-12-18 13:06:06 -0300

    feat: :art: add environment...

--- pushDocker.sh
+++ pushDocker.sh
@@ -7,7 +7,7 @@ docker image prune -f
 
 echo -e "\n\t------------\t BUILD\t| hostname: $(hostname) \t date: $(date) \t------------\n"
 docker pull n8nio/n8n:latest
-CURRENT_VERSION=$(docker image inspect n8nio/n8n:latest 2>/dev/null | jq -r '.[0].Config.Labels."org.opencontainers.image.version" // empty + "-" + .[0].Architecture')
+CURRENT_VERSION=$(docker image inspect n8nio/n8n:latest 2>/dev/null | jq -r '.[0].Config.Labels."org.opencontainers.image.version" + "-" + .[0].Architecture' // empty)
 docker build --build-arg BUILD_TAG_CUSTOM=v$CURRENT_VERSION -t afimpelcom/n8n-server-bash:v$CURRENT_VERSION -t afimpelcom/n8n-server-bash:latest -f Dockerfile . --force-rm
 
 echo -e "\n\t------------\t PUSH\t| hostname: $(hostname) \t date: $(date) \t------------\n"
